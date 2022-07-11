933ad4c0747fdadf8f916c04d2b294e45ba63e66
roosterdesign <neil@roosterdesign.co.uk>
2022-07-11 10:28:21 +0100

    dev

--- wp-content/themes/harringtons/templates/homepage.php
+++ wp-content/themes/harringtons/templates/homepage.php
@@ -5,14 +5,15 @@ Template Name: Homepage
 ?>
 <?php get_header(); ?>
 <div class="page">
-  <?php include get_theme_file_path("templates/partials/hero.php"); ?>  
-  <?php include get_theme_file_path("templates/partials/reviews-panel.php"); ?>    
-  <?php include get_theme_file_path("templates/partials/reservations-panel.php"); ?>    
-  <?php include get_theme_file_path("templates/partials/offers-panel.php"); ?>    
-  <?php include get_theme_file_path("templates/partials/menu-panel.php"); ?>    
-  <?php include get_theme_file_path("templates/partials/gallery.php"); ?>    
-  <?php include get_theme_file_path("templates/partials/functions-panel.php"); ?>    
-  <?php include get_theme_file_path("templates/partials/full-width-image.php"); ?>    
-  <?php include get_theme_file_path("templates/partials/instagram-panel.php"); ?>    
+  <?php include get_theme_file_path("templates/partials/hero.php"); ?>
+  <?php include get_theme_file_path("templates/partials/reviews-panel.php"); ?>
+  <?php include get_theme_file_path("templates/partials/reservations-panel.php"); ?>
+  <?php include get_theme_file_path("templates/partials/offers-panel.php"); ?>
+  <?php include get_theme_file_path("templates/partials/about-panel.php"); ?>
+  <?php include get_theme_file_path("templates/partials/menu-panel.php"); ?>
+  <?php include get_theme_file_path("templates/partials/gallery.php"); ?>
+  <?php include get_theme_file_path("templates/partials/functions-panel.php"); ?>
+  <?php include get_theme_file_path("templates/partials/full-width-image.php"); ?>
+  <?php include get_theme_file_path("templates/partials/instagram-panel.php"); ?>
 </div>
 <?php get_footer(); ?>
---
+++ wp-content/themes/harringtons/templates/partials/about-panel.php
@@ -0,0 +1,27 @@
+<?php
+  $pageId = 831; // DEV
+  // $pageId = 495; PROD
+  $args = array('p' => $pageId, 'post_type' => 'content-blocks'); $the_query = new WP_Query( $args );
+  if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post();
+  
+  $leftImage = get_field('left_image')['sizes'][ 'fw-img-desktop-lg' ];
+  $rightImage = get_field('right_image')['sizes'][ 'fw-img-desktop-lg' ];
+
+?>
+
+  <section class="about-panel">
+    <div class="container">
+
+      <div class="about-panel__content">
+        <p class="about-panel__subHead"><?php the_field('content_intro'); ?></p>
+        <?php the_field('content'); ?>
+      </div>
+
+    </div>
+
+    <div class="about-panel__leftImg" style="background-image: url(<?php echo $leftImage; ?>)"></div>
+    <div class="about-panel__rightImg" style="background-image: url(<?php echo $rightImage; ?>)"></div>
+
+  </section>
+
+<?php endwhile; wp_reset_postdata(); endif; ?>
